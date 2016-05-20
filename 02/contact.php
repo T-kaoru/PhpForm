1c4c9377129e9e92a19650ecf73c66395f931af1
T-kaoru <kkk73037@outlook.jp>
2016-05-20 18:03:23 +0900

    20

--- 02/contact.php
+++ 02/contact.php
@@ -7,32 +7,75 @@
 </head>
 
 <body>
-  <h1 id="title">お問い合わせ入力フォーム</h1>
+    <?php
+        // サニタイジング処理。一括処理を目的とした再起処理関数。
+        function sany($a){
+            $_data = array();
+            foreach ($a as $key => $value) {
+                if (is_array($value)) {
+                    $_data[$key] = sany($value);
+                }else{
+                    $_data[$key] = htmlspecialchars($value);
+                }
+            }
+            return $_data;
+        }
+        function error_check($a){
+            switch ($a) {
+                case 1:
+                    echo "未入力です";
+                    break;
+                case 2:
+                    echo "不正な値です";
+                    break;
+                case 3:
+                    echo "は？";
+                    break;
+                default:
+                    break;
+            }
+        }
+        // var_dump($_POST);
+     ?>
+  <h1 id="title">お問い合わせ 入力フォーム</h1>
 
   <div id="messeages">
       <p>
           <div id="default_msg">
-              意見ください。</br>
-              お願いします。</br>
+              意見ください。お願いします。</br>
+              必須マーク付いてる部分は必ず入力しないと怒られます。
           </div>
           <?php
+          //エラーチェックとエラー文出力PHP。OKだったら通過できる。
                 if( isset( $_POST['submit'] )){
                     echo '<div id="error_msg">';
+                    $error = array();
                     $error_flg = 0;
+
+                    //nameチェック
                     if(!strlen($_POST['name1']) || !strlen($_POST['name2'])){
-                        $error_flg = 1;
-                        echo "名前が未入力です。</br>";
+                        $error['name'] = 1;
                     }
                     if(!isset( $_POST['sexual'] )){
-                        $error_flg = 1;
-                        echo "性別を選択してください。</br>";
+                        $error['saxual'] = 1;
                     }
                     if(!strlen($_POST['mail1']) || !strlen($_POST['mail2'])){
-                        $error_flg = 1;
-                        echo "メールアドレスが未入力です。</br>";
+                        $error['mail'] = 1;
+                    }
+                    //エラーチェック判定。
+                    foreach ($error as $key => $value) {
+                        if ($value > 0){
+                            $error_flg = 1;
+                            break;
+                        }
                     }
                     if ( $error_flg === 0 ){
-                        header("Location: http://192.168.1.101/php/PhpForm/02/result.php");
+                        session_start();
+                        $_SESSION = sany($_POST);
+                        header("Location: ./result.php");
+                        exit;
+                    }else{
+                        echo "未入力、または入力に誤りがある項目があります！";
                     }
                     echo '</div>';
                 }
@@ -44,9 +87,9 @@
     <form action="contact.php" method="post">
       <div id="form">
         <p>
-          <class id="sub_title">お名前</class><class id="cns">必須</class>
+          <class id="sub_title">お名前</class><class id="cns">必須</class></br>
           <div id="in_form">
-            姓:<input type="textbox" name="name1" size="10" placeholder="田中">
+            姓:<input type="textbox" name="name1" size="10" required placeholder="田中">
             <?php
                 if( isset($_POST['submit']) && !strlen($_POST['name1'])){
                     echo "ここ";
@@ -54,7 +97,7 @@
              ?>
             <!-- セイ<input type="textbox" name="kana1" size="6"　placeholder="タナカ"> -->
           </br>
-            名:<input type="textbox" name="name2" size="10" placeholder="太郎">
+            名:<input type="textbox" name="name2" size="10" required placeholder="太郎">
             <?php
                 if( isset($_POST['submit']) && !strlen($_POST['name2'])){
                     echo "ここ";
@@ -72,7 +115,7 @@
           <div id="in_form">
             <input type="radio" name="sexual" id="sexual1" value="男" /><label for="sexual1">:男</lavel>
             <input type="radio" name="sexual" id="sexual2" value="女" /><label for="sexual2">:女</lavel>
-            <input type="radio" name="sexual" id="sexual3" value="不明" /><label for="sexual3">:不明</lavel>
+            <input type="radio" name="sexual" id="sexual3" value="不明" required /><label for="sexual3">:不明</lavel>
                 <?php
                     if( isset($_POST['submit']) && !strlen($_POST['name2'])){
                         echo "両性だった？";
@@ -85,10 +128,11 @@
       <!-- 住所入力フォーム  -->
       <div id="form">
         <p>
-          <class id="sub_title">郵便番号</class>
+          <class id="sub_title">郵便番号</class></br>
+          <class id="desc">3ケタ、4ケタの数字をそれぞれ入力してください。</class>
           <div id="in_form">
-            <input type="textbox" name="post1" size="1" placeholder="100">
-            - <input type="textbox" name="post2" size="1" placeholder="0005">
+            <input type="textbox" name="post1" size="1" pattern="\d{3}" placeholder="100">
+            - <input type="textbox" name="post2" size="1" pattern="\d{4}" placeholder="0005">
           </div>
           </br>
           <class id="sub_title">住所</class>
@@ -104,9 +148,9 @@
         <p>
           <class id="sub_title">電話番号</class>
           <div id="in_form">
-            <input type="textbox" name="phone1" size="1" placeholder="03">
-            - <input type="textbox" name="phone2" size="1" placeholder="3286">
-            - <input type="textbox" name="phone3" size="1" placeholder="7887">
+            <input type="textbox" name="phone1" size="1" pattern="\d{2,4}" placeholder="03">
+            ( <input type="textbox" name="phone2" size="1" pattern="\d{3,4}" placeholder="3286">
+            ) <input type="textbox" name="phone3" size="1" pattern="\d{3,4}" placeholder="7887">
           </div>
         </p>
       </div>
@@ -116,8 +160,8 @@
         <p>
           <class id="sub_title">メールアドレス</class><class id="cns">必須</class>
           <div id="in_form">
-            <input type="textbox" name="mail1" size="20" placeholder="sample">
-            @ <input type="textbox" name="mail2" size="20" placeholder="example.com">
+            <input type="textbox" name="mail1" size="20" required placeholder="sample">
+            @ <input type="textbox" name="mail2" size="20" required placeholder="example.com">
             <?php
                 if( isset($_POST['submit'])){
                     if(!strlen($_POST['mail1']) && !strlen($_POST['mail2'])){
@@ -152,7 +196,8 @@
           <p>
             <class id="sub_title">どういった用件での問い合わせかね</class><class id="cns">必須</class>
             <div id="in_form">
-              <select name="select">
+              <select name="select" required>
+                  <option value="select0">-- 選択してください --</option>
                 <option value="select1">いい子だねー</option>
                 <option value="select2">いい子でちゅね～</option>
                 <option value="select3">ｱｰ、ﾖｼﾖｼﾖｼ</option>
@@ -161,7 +206,7 @@
           </br>
             <class id="sub_title">書いてねー。</class><class id="cns">必須</class>
             <div id="in_form">
-              <textarea cols="80" rows="15" name="textarea" placeholder="ご意見、ご感想をお聞かせください。"></textarea>
+              <textarea cols="80" rows="15" name="textarea" required placeholder="ご意見、ご感想をお聞かせください。"></textarea>
             </div>
           </p>
       </div>
--- 02/result.php
+++ 02/result.php
@@ -2,13 +2,21 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>入力フォーム</title>
+    <title>老人ホーム ～來來軒～ [内容確認]</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-
-    <h1 id="title">お問い合わせ入力フォーム</h1>
+    <?php
+        //セッション呼び出し
+        session_start();
+        $data = array();
+        foreach ($_SESSION as $key => $value) {
+            $data[$key] = $value;
+        }
+        // var_dump($data);
+     ?>
+    <h1 id="title">お問い合わせ 内容確認</h1>
 
     <table border="1">
 
@@ -17,7 +25,7 @@
         <p>
           <class id="sub_title">お名前</class>
           <div id="in_form">
-            お名前 <?php echo $_POST['name1'] . " " . $_POST['name2']; ?>
+            <?php echo $data['name1'] . " " . $data['name2']; ?>
           </div>
         </p>
       </div>
@@ -27,7 +35,7 @@
         <p>
           <class id="sub_title">性別</class>
           <div id="in_form">
-            <?php echo $_POST['sexual'] ?>
+            <?php echo $data['sexual'] ?>
           </div>
         </p>
       </div>
@@ -37,8 +45,9 @@
         <p>
           <class id="sub_title">ご住所</class>
           <div id="in_form">
-            <?php echo "〒 " . $_POST['post1'] . "-" . $_POST['post2'] ?>
-            <?php echo $_POST['address'] ?>
+            <?php
+                // if( echo "〒 " . $data['post1'] . "-" . $data['post2'] ?>
+            <?php echo $data['address'] ?>
           </div>
         </p>
       </div>
@@ -48,7 +57,7 @@
         <p>
           <class id="sub_title">電話番号</class>
           <div id="in_form">
-            <?php echo $_POST['phone1'] . $_POST['phone2'] . $_POST['phone3'] ?>
+            <?php echo $data['phone1'] . $data['phone2'] . $data['phone3'] ?>
           </div>
         </p>
       </div>
@@ -58,7 +67,7 @@
         <p>
           <class id="sub_title">メールアドレス</class>
           <div id="in_form">
-            <?php echo $_POST['mail1'] . "@" . $_POST['mail2'] ?>
+            <?php echo $data['mail1'] . "@" . $data['mail2'] ?>
           </div>
         </p>
       </div>
@@ -70,7 +79,7 @@
             よろしければ、どの点にご不満があったのかお聞かせください(複数回答可)
           </class>
           <div id="in_form">
-            <?php echo $_POST['box'] ?>
+            <?php echo $data['box'] ?>
           </div>
         </p>
       </div>
@@ -80,12 +89,12 @@
           <p>
             <class id="sub_title">どういった用件での問い合わせかね</class>
             <div id="in_form">
-              <?php echo $_POST['select'] ?>
+              <?php echo $data['select'] ?>
             </div>
           </br>
             <class id="sub_title">書いてねー。</class>
             <div id="in_form">
-              <?php echo $_POST['textarea'] ?>
+              <?php echo $data['textarea'] ?>
             </div>
           </p>
       </div>
